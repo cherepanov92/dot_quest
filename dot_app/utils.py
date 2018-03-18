43b2a268fc11f334b4e298505bab68269bd04e0e
Biowulf513 <cherepanov92@gmail.com>
2018-03-19 00:45:27 +0500

    парсинг 10 страниц типа insider-trades

--- dot_app/utils.py
+++ dot_app/utils.py
@@ -14,11 +14,9 @@ class Utils:
             line_list = []
             for line in f:
                 line_list.append(line.rstrip())
-
             return line_list
 
-    # Парсер
-
+    # Парсинг компании
     def historical_parser(self, company):
         # запрос структуры страницы
         page = requests.get(f'https://www.nasdaq.com/symbol/{company}/historical')
@@ -60,21 +58,42 @@ class Utils:
                         volume = info_list[5]
                         ))
             del info_list[:6]
-        company_dict = dict(company=dict(short_name = company),
-                           info = info_dict)
+        company_dict = dict(
+                            company=dict(short_name = company),
+                            info = info_dict
+                            )
         return company_dict
 
-    def insider_trades_parser(self, company):
+    # Объединение информации о нескольких компаниях
+    def all_historical(self):
+        all_historical = []
+
+        # парсим страницы для всех акций из списка
+        for company in Utils.read_tickers():
+            all_historical.append(self.historical_parser(company))
+
+        # Отдаём всю информацию JSON-ом
+        return json.dumps(all_historical, sort_keys=True, indent=4)
+
+    # Парсинг одной страницы insider_trades
+    def insider_trades_parser(self, url):
         # запрос структуры страницы
-        page = requests.get(f'https://www.nasdaq.com/symbol/{company}/insider-trades')
+        page = requests.get(url)
         tree = html.fromstring(page.content)
 
         # выборка информации с помощью xpath
         xpath_selection = list(tree.xpath(".//*[@class='genTable']/table/tr/td//text()"))
         info_list = []
         while len(xpath_selection):
+            if len(xpath_selection)<8 or xpath_selection[7].isupper():
+                xpath_selection.insert(6,0)
             temp_list = xpath_selection[:8]
             del xpath_selection[:8]
+
+            # Форматирование даты
+            info_date = temp_list.pop(2).split('/')
+            clear_data = '{YYYY}-{MM}-{DD}'.format(YYYY=info_date[2], MM=info_date[0], DD=info_date[1])
+            temp_list.insert(2, clear_data)
 
             info_list.append(dict(
                 insider = temp_list[0],
@@ -86,28 +105,32 @@ class Utils:
                 last_price = temp_list[6],
                 shares_held = temp_list[7]
             ))
+        return info_list
 
-        print(info_list)
-
-    def all_historical(self):
-        all_historical = []
-
-        # парсим страницы для всех акций из списка
-        for company in Utils.read_tickers():
-            all_historical.append(self.historical_parser(company))
-
-        # Отдаём всю информацию JSON-ом
-        return json.dumps(all_historical, sort_keys=True, indent=4)
+    # Объединение информации о всех страницах insider_trades
+    def collect_insider_trades(self, company):
+        all_insider_trades = []
+        for i in range(1,10):
+            url = f'https://www.nasdaq.com/symbol/{company}/insider-trades?page={i}'
+            try:
+                info = self.insider_trades_parser(url)
+                '''
+                Если дата последней страницы равна дате предпоследней страницы 
+                это означает что прошлая страница была последней (информативной).
+                Так-как сайт отдаёт страницу с последнем id даже если в id передаётся 
+                значение больше самой старой страницы
+                '''
+                if all_insider_trades and info[-1]['last_date'] == all_insider_trades[-1][-1]['last_date']:
+                    print('повтор инфориации')
+                    break
+                # print(info[-1]['last_date'])
+                all_insider_trades.append(info)
+            except Exception as error:
+                print('ERROR', error)
+        # print(all_insider_trades[-1][-1]['last_date'])
+        return json.dumps(all_insider_trades, sort_keys=True, indent=4)
 
 if __name__ == '__main__':
     i = Utils()
-
-    # print(i.read_tickers())
-
-
-    # for info in i.parser():
-    #     print(info)
-
-    i.triders_parser('aapl')
-
+    i.collect_insider_trades('aapl')
     pass
