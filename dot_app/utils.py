18fceb398614cc76d41a02bd00a4a5bc4724f5f9
Biowulf513 <cherepanov92@gmail.com>
2018-03-19 21:51:13 +0500

    многопоточная обработка реализация для страиц типа handle и insider_trades

--- dot_app/utils.py
+++ dot_app/utils.py
@@ -137,7 +137,16 @@ class Utils:
         return json.dumps(all_info, sort_keys=True, indent=4)
 
 ############################################################################
+    # Парсер
+    def parser(self, page_dict):
+        if page_dict['type'] == 'insider-trades':
+            self.insider_trades(page_dict)
+        elif page_dict['type'] == 'historical':
+            self.historical(page_dict)
+        else:
+            print('error')
 
+    # Генератор списка из url и сопутствующей информации
     def url_generator(self):
         all_company = Utils.read_tickers()
         for company in all_company:
@@ -146,13 +155,23 @@ class Utils:
                        url = f'https://www.nasdaq.com/symbol/{company}/historical'
                        )
 
-            for i in range(1,11):
+            for i in range(1,4):
                 yield dict(company_name = company,
                            type = 'insider-trades',
                            url = f'https://www.nasdaq.com/symbol/{company}/insider-trades?page={i}'
                            )
 
-    # Парсинг компании
+    # Форматирование request Даты
+    def format_data(self, date_value):
+        # Если в ячейке даты лежит время заменяем его на текущую дату
+        if len(date_value) < 10:
+            return datetime.today().strftime('%Y-%m-%d')
+        # Иначе форматируем данные
+        else:
+            date_list = date_value.split('/')
+            return '{YYYY}-{MM}-{DD}'.format(YYYY=date_list[2], MM=date_list[0], DD=date_list[1])
+
+    # Обработка historical request
     def historical(self, page_dict):
 
         # запрос структуры страницы
@@ -187,22 +206,37 @@ class Utils:
 
         print(company_dict)
 
-    def format_data(self, date_value):
-        # Если в ячейке даты лежит время заменяем его на текущую дату
-        if len(date_value) < 10:
-            return datetime.today().strftime('%Y-%m-%d')
-        # Иначе форматируем данные
-        else:
-            date_list = date_value.split('/')
-            return '{YYYY}-{MM}-{DD}'.format(YYYY=date_list[2], MM=date_list[0], DD=date_list[1])
+    # Обработка insider_trades request
+    def insider_trades(self, page_dict):
+        # запрос структуры страницы
+        page = requests.get(page_dict['url'])
+        tree = html.fromstring(page.content)
 
-    def parser(self, page_dict):
-        if page_dict['type'] == 'insider-trades':
-            print('insider-trades')
-        elif page_dict['type'] == 'historical':
-            self.historical(page_dict)
-        else:
-            print('error')
+        # выборка информации с помощью xpath
+        xpath_selection = list(tree.xpath(".//*[@class='genTable']/table/tr/td//text()"))
+        info_dict = []
+        while len(xpath_selection):
+            if len(xpath_selection)<8 or xpath_selection[7].isupper():
+                xpath_selection.insert(6,0)
+            temp_list = xpath_selection[:8]
+            del xpath_selection[:8]
+
+            info_dict.append(dict(
+                insider_name = temp_list[0],
+                relation = temp_list[1],
+                last_date = self.format_data(temp_list[2]),
+                trans_type = temp_list[3],
+                owner_type = temp_list[4],
+                shares_traded = re.sub('[,]', '',temp_list[5]),
+                last_price = temp_list[6],
+                shares_held = re.sub('[,]', '',temp_list[7])
+            ))
+        company_dict = dict(company=dict(short_name=page_dict['company_name']),
+                            type='insider-trades',
+                            info=info_dict
+                            )
+
+        print(company_dict)
 
 
 if __name__ == '__main__':
