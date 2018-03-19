8dbee779a5044ad157bdc51451ddfc44f91b97c1
Biowulf513 <cherepanov92@gmail.com>
2018-03-19 22:54:53 +0500

    многопоточная обработка сохранение данных в DB для json типа handle

--- dot_app/management/commands/parser.py
+++ dot_app/management/commands/parser.py
@@ -13,74 +13,87 @@ from multiprocessing import Pool
 class Command(BaseCommand):
     help = 'Парсинг'
 
-    def company_in_DB(self, company):
-
-        if Company.objects.filter(company_alias = company['short_name']).exists():
-            exist_company = Company.objects.get(company_alias = company['short_name'])
-            return exist_company.id
-        else:
-            new_company = Company(company_alias = company['short_name'])
-            new_company.save()
-            return new_company.id
-
-    def insider_in_db(self, insider):
-        if Insider.objects.filter(name = insider['insider_name']).exists():
-            exist_insider = Insider.objects.get(name = insider['insider_name'])
-            return exist_insider.id
-        else:
-            new_insider = Insider(name = insider['insider_name'], relation = insider['relation'])
-            new_insider.save()
-            return new_insider.id
-
     def add_arguments(self, parser):
         parser.add_argument('multi_thread_col', nargs='+', type=int)
 
     def handle(self, *args, **options):
-            message = Utils()
-            # data_json = json.loads(message.collect_insider_trades())
-            # print(data_json)
-            # for company_info in data_json:
-            #     company_id = self.company_in_DB(company_info['company'])
-            #     if company_info['type'] == 'historical':
-            #         for one_record in company_info['info']:
-            #             Historical.objects.bulk_create([
-            #                 Historical(
-            #                     company_alias_id = company_id,
-            #                     close = one_record['close'],
-            #                     date = one_record['date'],
-            #                     open = one_record['open'],
-            #                     high = one_record['high'],
-            #                     low = one_record['low'],
-            #                     volume = one_record['volume']
-            #                 )
-            #             ])
-            #     elif company_info['type'] == 'insider_trades':
-            #         print('===insider_trades===')
-            #
-            #         for company_record in company_info['info']:
-            #             for one_record in company_record:
-            #                 insider_id = self.insider_in_db(one_record)
-            #                 print(insider_id, one_record['insider_name'], one_record['relation'], one_record['shares_held'])
-            #
-            #                 InsiderTrades.objects.bulk_create([
-            #                     InsiderTrades(
-            #                         company_alias_id = company_id,
-            #                         insider_id = insider_id,
-            #                         last_date = one_record['last_date'],
-            #                         trans_type = one_record['trans_type'],
-            #                         owner_type = one_record['owner_type'],
-            #                         shares_traded = one_record['shares_traded'],
-            #                         last_price = one_record['last_price'],
-            #                         shares_held = one_record['shares_held']
-            #                     )
-            #                 ])
-########################################################################################################################
-            # for i in message.url_generator():
-            #     print(i)
+            parser = Utils()
 
-            with Pool(4) as p:
-                p.map(message.parser, message.url_generator())
+            with Pool(10) as p:
+                for json_request in (p.map(parser.parser, parser.url_generator())):
+                    self.json_parser(json_request)
 
+    def company_in_DB(self, company):
+
+        if Company.objects.filter(company_alias=company).exists():
+            exist_company = Company.objects.get(company_alias=company)
+            return exist_company.id
+        else:
+            new_company = Company(company_alias=company)
+            new_company.save()
+            return new_company.id
+
+    def insider_in_db(self, insider):
+        if Insider.objects.filter(name=insider['insider_name']).exists():
+            exist_insider = Insider.objects.get(name=insider['insider_name'])
+            return exist_insider.id
+        else:
+            new_insider = Insider(name=insider['insider_name'], relation=insider['relation'])
+            new_insider.save()
+            return new_insider.id
+
+    def json_parser(self, json_data):
+        data_json = json.loads(json_data)
+        print(data_json, '\n')
+
+        company_id = self.company_in_DB(data_json['company']['short_name'])
+        if data_json['type'] == 'historical':
+            for company_info in data_json['info']:
+                print(company_info)
+                Historical.objects.bulk_create([
+                    Historical(
+                        company_alias_id = company_id,
+                        close = company_info['close'],
+                        date = company_info['date'],
+                        open = company_info['open'],
+                        high = company_info['high'],
+                        low = company_info['low'],
+                        volume = company_info['volume']
+                    )
+                ])
+                # print('{type}{company_name} append'.format(type = company_info['type'], company_name = company_info['company']))
+        # elif data_json['type'] == 'insider-trades':
+        #     print('===insider_trades===')
+        #     for company_record in data_json['info']:
+        #         print(company_record)
+
+
+                # for one_record in company_record:
+
+                    # insider_id = self.insider_in_db(one_record)
+                    # print(insider_id, one_record['insider_name'], one_record['relation'], one_record['shares_held'])
+                    #
+                    # InsiderTrades.objects.bulk_create([
+                    #     InsiderTrades(
+                    #         company_alias_id = company_id,
+                    #         insider_id = insider_id,
+                    #         last_date = one_record['last_date'],
+                    #         trans_type = one_record['trans_type'],
+                    #         owner_type = one_record['owner_type'],
+                    #         shares_traded = one_record['shares_traded'],
+                    #         last_price = one_record['last_price'],
+                    #         shares_held = one_record['shares_held']
+                    #     )
+                    # ])
+#
+#
+#
+#
+#
+#
+# # if __name__ == '__main__':
+# #     i = Command()
+# #     i.pool()
             # url =>| pool => html => info => json => DB
             '''
             чтение из файла
--- dot_app/utils.py
+++ dot_app/utils.py
@@ -8,154 +8,32 @@ from datetime import datetime
 class Utils:
 
     # Чтение файла с названиями акций
-    @staticmethod
-    def read_tickers():
+    def read_tickers(self):
         with open('tickers.txt', mode='r') as f:
             line_list = []
             for line in f:
                 line_list.append(line.rstrip())
             return line_list
 
-    # Парсинг компании
-    def historical_parser(self, company):
-        # запрос структуры страницы
-        page = requests.get(f'https://www.nasdaq.com/symbol/{company}/historical')
-        tree = html.fromstring(page.content)
-
-        # выборка информации с помощью xpath
-        page_table = tree.xpath(".//div[@id='quotes_content_left_pnlAJAX']/table/tbody/tr/td/text()")
-
-        # Конкатенация полученных данных
-        table_info = (''.join(page_table))
-
-        # превидение данных к списку
-        info_list = (re.findall(r'\s+(\S+)\s', re.sub('[,]', '',table_info)))
-        info_dict = []
-
-        while len(info_list):
-            # Если в ячейке даты лежит время заменяем его на текущую дату
-            if len(info_list[0]) < 10:
-                    info_list.pop(0)
-                    info_list.insert(0, datetime.today().strftime('%Y-%m-%d'))
-            else:
-                # Форматирование даты
-                info_date = info_list.pop(0).split('/')
-                clear_data = '{YYYY}-{MM}-{DD}'.format(YYYY=info_date[2],MM=info_date[0],DD=info_date[1])
-                info_list.insert(0, clear_data)
-
-            info_dict.append(dict(
-                        date = info_list[0],
-                        open = info_list[1],
-                        high = info_list[2],
-                        low = info_list[3],
-                        close = info_list[4],
-                        volume = info_list[5]
-                        ))
-            del info_list[:6]
-        company_dict = dict(
-                            company=dict(short_name = company),
-                            type = 'historical',
-                            info = info_dict
-                            )
-        return company_dict
-
-    # Объединение информации о нескольких компаниях
-    def all_historical(self):
-        all_historical = []
-
-        # парсим страницы для всех акций из списка
-        for company in Utils.read_tickers():
-            all_historical.append(self.historical_parser(company))
-
-        # Отдаём всю информацию JSON-ом
-        return json.dumps(all_historical, sort_keys=True, indent=4)
-
-    # Парсинг одной страницы insider_trades
-    def insider_trades_parser(self, url):
-        # запрос структуры страницы
-        page = requests.get(url)
-        tree = html.fromstring(page.content)
-
-        # выборка информации с помощью xpath
-        xpath_selection = list(tree.xpath(".//*[@class='genTable']/table/tr/td//text()"))
-        info_dict = []
-        while len(xpath_selection):
-            if len(xpath_selection)<8 or xpath_selection[7].isupper():
-                xpath_selection.insert(6,0)
-            temp_list = xpath_selection[:8]
-            del xpath_selection[:8]
-
-            # Форматирование даты
-            info_date = temp_list.pop(2).split('/')
-            clear_data = '{YYYY}-{MM}-{DD}'.format(YYYY=info_date[2], MM=info_date[0], DD=info_date[1])
-            temp_list.insert(2, clear_data)
-
-            info_dict.append(dict(
-                insider_name = temp_list[0],
-                relation = temp_list[1],
-                last_date = temp_list[2],
-                trans_type = temp_list[3],
-                owner_type = temp_list[4],
-                shares_traded = re.sub('[,]', '',temp_list[5]),
-                last_price = temp_list[6],
-                shares_held = re.sub('[,]', '',temp_list[7])
-            ))
-        return info_dict
-
-    # Объединение информации о всех страницах insider_trades
-    def collect_insider_trades(self):
-        all_info = []
-        company_array = Utils.read_tickers()
-        for company in company_array:
-            info_dict = []
-            for i in range(1,11):
-                url = 'https://www.nasdaq.com/symbol/{company}/insider-trades?page={i}'.format(company = company.lower(), i = i)
-                print(url)
-                try:
-                    info = self.insider_trades_parser(url)
-                    print(info[-1])
-                    '''
-                    Если дата последней страницы равна дате предпоследней страницы
-                    это означает что прошлая страница была последней (информативной).
-                    Так-как сайт отдаёт страницу с последнем id даже если в id передаётся
-                    значение больше самой старой страницы
-                    '''
-                    if info_dict and info[-1] == info_dict[-1][-1]:
-                        print('повтор инфориации')
-                        break
-                    print(info[-1]['last_date'])
-                    info_dict.append(info)
-                except Exception as error:
-                    print('ERROR', error)
-                print(len(info_dict))
-
-            all_info.append(dict(
-                company=dict(short_name=company),
-                type='insider_trades',
-                info=info_dict))
-
-        return json.dumps(all_info, sort_keys=True, indent=4)
-
-############################################################################
     # Парсер
     def parser(self, page_dict):
         if page_dict['type'] == 'insider-trades':
-            self.insider_trades(page_dict)
+            return(self.insider_trades(page_dict))
         elif page_dict['type'] == 'historical':
-            self.historical(page_dict)
+            return(self.historical(page_dict))
         else:
             print('error')
 
     # Генератор списка из url и сопутствующей информации
     def url_generator(self):
-        all_company = Utils.read_tickers()
+        all_company = self.read_tickers()
         for company in all_company:
             yield dict(company_name = company,
                        type = 'historical',
                        url = f'https://www.nasdaq.com/symbol/{company}/historical'
                        )
 
-            for i in range(1,4):
+            for i in range(1,11):
                 yield dict(company_name = company,
                            type = 'insider-trades',
                            url = f'https://www.nasdaq.com/symbol/{company}/insider-trades?page={i}'
@@ -204,7 +82,7 @@ class Utils:
                             info=info_dict
                             )
 
-        print(company_dict)
+        return json.dumps(company_dict, sort_keys=True, indent=4)
 
     # Обработка insider_trades request
     def insider_trades(self, page_dict):
@@ -236,7 +114,7 @@ class Utils:
                             info=info_dict
                             )
 
-        print(company_dict)
+        return json.dumps(company_dict, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
