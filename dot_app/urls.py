a0b685f02a35d4db07685ba5e774a5943472e3dc
Biowulf513 <cherepanov92@gmail.com>
2018-03-21 22:09:40 +0500

    веб-интерфейс, который на /%TICKER/delta?value=N&type=(open/high/low/close) отдаёт веб-страницу с данными о минимальных периодах (дата начала-дата конца), когда указанная цена изменилась более чем на N

--- dot_app/urls.py
+++ dot_app/urls.py
@@ -7,11 +7,17 @@ from django.conf.urls import url
 from .views import  historical, insider, insider_name, analytics, delta
 
 urlpatterns = [
+
     url(r'^$', historical, name="historical"),
+    url(r'^api/$', historical,{'api': True}, name="historical"),
     url(r'^insider$', insider, name="insider"),
-    url(r'^insider/(?P<insider_name>[A-Za-z0-9 ]+)$', insider_name, name="insider_name"),
+    url(r'^api/insider$', insider, {'api': True}, name="insider"),
+    url(r'^insider/(?P<insider_name>[A-Za-z0-9. ]+)$', insider_name, name="insider_name"),
+    url(r'^api/insider/(?P<insider_name>[A-Za-z0-9. ]+)$', insider_name, {'api': True}, name="insider_name"),
     url(r'^analytics$', analytics, name="analytics"),
+    url(r'^api/analytics$', analytics, {'api': True}, name="analytics"),
     url(r'^delta$', delta, name="delta"),
+    url(r'^api/delta$', delta, {'api': True}, name="delta"),
 ]
 
 # http://127.0.0.1:8000/goog/analytics/analytics?date_from=2018-01-22&date_to=2018-01-11
